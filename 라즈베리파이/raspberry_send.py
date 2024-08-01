98641ecc3d93337ed83ab432d7548e3170a8ea50
BJP <oyoungeric@gmail.com>
2024-08-01 14:45:50 +0900

    asf

--- 라즈베리파이/raspberry_send.py
+++ 라즈베리파이/raspberry_send.py
@@ -15,18 +15,18 @@ server_url = "http://192.168.20.230:5001/upload"
 
 audio = pyaudio.Pyaudio() # 오디오 객체 생성
 
-i = 0 # 녹음 카운트
+index_count = 0 # 녹음 카운트
 #반복해서 전송
 while True:
     try:
         #진동 체크
-        vibrate_server_main2()
-        vibrate_server_main3()
+        # vibrate_server_main2()
+        # vibrate_server_main3()
 
         #스트림 시작
         stream = audio.open(format = Format, channels = Channels, rate = Rate, input = True, frames_per_buffer = Chunk)
 
-        print(f"{i}번째 녹음중입니다...")
+        print(f"{index_count}번째 녹음중입니다...")
 
         frames = [] #오디오 정보를 담을 리스트
         
@@ -48,6 +48,8 @@ while True:
             print("전송 완료")
         else:
             print("전송 실패")
+        
+        index_count +=1
             
     finally:
         audio.terminate()
